06e99c6a0d79a278d1d6180484d28700bb44d549
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-08 10:36:33 +0000

    set up try except statement to find artist id

--- music_artists.py
+++ music_artists.py
@@ -3,3 +3,12 @@ class Artist:
     def __init__(self, spotify_credentials, name):
         self.spotify = spotify_credentials
         self.name = name
+        self.id = self.artist_id()
+
+    def artist_id(self):
+        results = self.spotify.search(q='artist:' + self.name, type='artist')
+        narrowing_down_artist_details = results['artists']
+        try:
+            final_artist = narrowing_down_artist_details['items'][0]
+            music_artist_spotify_id = final_artist['id']
+        return music_artist_spotify_id
