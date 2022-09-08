8fda162d661958e4da13672ab2599c39878995e2
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-08 12:12:29 +0000

    add update docstring for functions in main()

--- run.py
+++ run.py
@@ -4,21 +4,29 @@ from client_details import *
 from music_artists import *
 
 def run_spotify(client_id, client_secret):
-    '''Access the Spotify API with a client ID and client secret'''
+    '''
+    Access the Spotify API with a client ID and client secret
+    '''
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
                                                         client_secret=client_secret)
     spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     return spotify
 
-def closed_question_answer_checks(Y_or_N):
-    '''Checks if the user inputs a valid 'Y' or 'N' value into the terminal'''
-    remove_whitespace = Y_or_N.replace(' ', '')
+def closed_question_answer_checks(y_or_n):
+    '''
+    Checks if the user inputs a valid y (yes) or n (no) value into the terminal
+    '''
+    remove_whitespace = y_or_n.replace(' ', '')
     while remove_whitespace.isalpha() == False or remove_whitespace.lower() not in ['y','n']:
         remove_whitespace = input('\nAnswer not valid. Please enter Y or N:\n')
         remove_whitespace.replace(' ', '')
     return remove_whitespace.lower()
 
 def select_artists(spotify):
+    '''
+    Takes the spotify connection as an argument and 
+    gets the spotify IDs for the music artists called
+    '''
     music_artists = []
     print('First of all, we need to know who your favourite music artists'\
             'are at the moment?\n\nUp to 5!\n\n'\
@@ -41,9 +49,12 @@ def select_artists(spotify):
 
 def main():
     spotify = run_spotify(CLIENT_ID, CLIENT_SECRET)
-    print('Are you looking for some new song recommendations?\nIf yes, then you\'ve come to the right place!\n\n')
-    print('In order to make some suitable recommendations, we just need to get to know you a bit better!\n\n')
+    print('Are you looking for some new song recommendations?\n'\
+        'If yes, then you\'ve come to the right place!\n\n')
+    print('In order to make some suitable recommendations, '\
+        'we just need to get to know you a bit better!\n\n')
     music_artists = select_artists(spotify)
-
+    print(music_artists)
+    print('next stage')
 
 main()
