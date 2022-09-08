3bcc0edd7e5d67b8da1e3c3611ac98c14a9ed6d4
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-08 10:04:11 +0000

    create main() for all functions

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from client_details import *
+
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
+                                                    client_secret=CLIENT_SECRET)
+spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+
+def main():
+    print('place functions here')
+
+main()
