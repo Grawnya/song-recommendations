f05997d131a15bc0f94e9f7a0f206784601f01d3
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-08 10:13:08 +0000

    create select_artists function

--- run.py
+++ run.py
@@ -2,12 +2,25 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from client_details import *
 
-client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID,
-                                                    client_secret=CLIENT_SECRET)
-spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+def run_spotify(client_id, client_secret):
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id,
+                                                        client_secret=client_secret)
+    spotify = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    return spotify
+
+def select_artists():
+    music_artists = []
+    print('First of all, we need to know who your favourite music artists'\
+            'are at the moment?\n\nUp to 5!\n\n'\
+            'Examples include: Cardi B, Arctic Monkeys and Bryan Adams\n\n'\
+            'Make sure you spell their name correctly\n\n')
 
 def main():
-    print('place functions here')
+    # place functions here
+    spotify = run_spotify(CLIENT_ID, CLIENT_SECRET)
+    print('Are you looking for some new song recommendations?\nIf yes, then you\'ve come to the right place!\n\n')
+    print('In order to make some suitable recommendations, we just need to get to know you a bit better!\n\n')
+
 
 main()
