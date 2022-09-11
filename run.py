5655757673229238224715a139605286209a5c1d
Grainne Donegan <grainnejdonegan@gmail.com>
2022-09-11 21:31:08 +0000

    create want_to_dance() to ask user question

--- run.py
+++ run.py
@@ -139,8 +139,23 @@ def song_selection(spotify):
     songs_string = ','.join([str(item) for item in songs])
     return songs_string
 
+def want_to_dance():
+    '''docstring'''
+    print('\n\nWe just need to ask a few more questions to pick out'\
+        '\nthe perfect songs for you!\n'\
+        'These ones are more mood based\n\n*******\n\n'
+    )
+    want_to_dance_question = input('1. Do you feel like dancing at the moment? Y or N\n')
+    danceability = closed_question_answer_checks(want_to_dance_question)
+    if danceability == 'y':
+        danceability = 'max_danceability'
+    else:
+        danceability = 'min_danceability'
+    return danceability
+
 def song_style_questions():
     '''docstring'''
+    
 
 def main():
     spotify = run_spotify(CLIENT_ID, CLIENT_SECRET)
